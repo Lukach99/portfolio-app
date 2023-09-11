baaacd973cbdc07038f42ce639ca61ad4ba3140a
private-account <lukac.tino@gmail.com>
2023-09-11 16:14:49 +0200

    Init commit

--- portfolio-app/src/app/app.component.spec.ts
+++ portfolio-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-wesbite'`, () => {
+  it(`should have as title 'portfolio-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-wesbite');
+    expect(app.title).toEqual('portfolio-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-wesbite app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-app app is running!');
   });
 });
